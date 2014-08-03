8dc2f3af40833d6a6f9a4f265aabdc1024fe73fd
r-lyeh <rlyeh.nospam@gmail.com>
2014-08-03 17:32:55 +0200

    Initial commit

---
+++ frodo.hpp
@@ -0,0 +1,23 @@
+// Frodo, a ring dependency system. boost licensed.
+// - rlyeh ~~ listening to stoned jesus / i'm the mountain
+
+#pragma once
+
+#include <map>
+#include <utility>
+#include <functional>
+#include <string>
+
+namespace frodo {
+    struct level {
+        std::string name;
+        std::function<bool()> init;
+        std::function<bool()> quit;
+    };
+
+    bool ring( int lvl, const level &impl );
+    bool init();
+    bool alive();
+    bool reboot( int lvl_target );
+    bool quit();
+}
